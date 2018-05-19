ab977c12daef6de76df1ee902b0877d418842b6c
MazeR <mazer0701@gmail.com>
2018-05-19 14:38:50 +0800

    add problem Algorithms - Two Sum - one-pass map

---
+++ Algorithms/01TwoSum/03.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> numsMap;
+        map<int, int> ::iterator jItr;
+        int numSize = nums.size();
+        for (int i = 0; i < numSize; i++) {
+            jItr = numsMap.find(target - nums[i]);
+            if (jItr != numsMap.end()) {
+                int j = jItr->second;
+                if (i != j) {
+                    return vector<int>{i, j};
+                }
+            } else {
+                numsMap[nums[i]] = i;
+            }
+        }
+    }
+};

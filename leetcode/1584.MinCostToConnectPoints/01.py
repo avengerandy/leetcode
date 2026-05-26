6077538d868360ac3f7959cc5db6cc66584477a9
mazer <mazer0701@gmail.com>
2026-05-26 22:46:47 +0800

    add 1584.MinCostToConnectPoints

---
+++ leetcode/1584.MinCostToConnectPoints/01.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adj = {i: [] for i in range(len(points))}
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i + 1, len(points)):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        res = 0
+        visit = set()
+        minH = [[0, 0]]
+        while len(visit) < len(points):
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            res = res + cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
